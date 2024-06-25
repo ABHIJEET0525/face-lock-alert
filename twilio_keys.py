20b930fe881caa2a75debac5cc7c715e0a38af3f
ABHIJEET0525 <168693135+ABHIJEET0525@users.noreply.github.com>
2024-06-25 19:26:53 +0530

    Add files via upload

---
+++ twilio_keys.py
@@ -0,0 +1,4 @@
+twilio_accountSID='Add your twilio account ID here'
+twilio_authToken='add twilio auth token'
+twilio_number='add allocated twilio number'
+destination_number='add destination phone number'
